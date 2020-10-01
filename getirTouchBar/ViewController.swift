6265b01869a84bd7a11e6d9d4a429a5537c28074
Muzaffer <muzaffer.yildirim@getir.com>
2020-10-02 00:00:12 +0300

    Initial Commit

---
+++ getirTouchBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  getirTouchBar
+//
+//  Created by Muzaffer Yıldırım on 2.10.2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
